395edb949f5996f785b6602780f92a75171107e4
Wamedera <mamede8463@outlook.com>
2022-11-26 21:31:39 -0300

    aula 104

--- todo_list/lib/pages/todo_list_page.dart
+++ todo_list/lib/pages/todo_list_page.dart
@@ -13,94 +13,94 @@ class _ToDoListPageState extends State<ToDoListPage> {
   final TextEditingController todoController = TextEditingController();
 
   List<Todo> todos = [];
+  Todo? deletedTodo;
+  int? deletedTodoPos;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: todoController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Adicione uma Tarefa',
-                          hintText: 'Ex.: Estudar Flutter',
-                        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: todoController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma Tarefa',
+                        hintText: 'Ex.: Estudar Flutter',
                       ),
                     ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        String text = todoController.text;
-                        setState(() {
-                          Todo newTodo = Todo(
-                            title: text,
-                            dateTime: DateTime.now(),
-                          );
-                          todos.add(newTodo);
-                        });
-                        todoController.clear();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xff00d7f3),
-                        padding: EdgeInsets.all(14),
-                      ),
-                      child: Icon(
-                        Icons.add,
-                        size: 30,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Flexible(
-                  child: ListView(
-                    shrinkWrap: true,
-                    children: [
-                      for (Todo todo in todos)
-                        TodoListItem(
-                          todo: todo,
-                          onDelete: onDelete,
-                        ),
-                    ],
                   ),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Row(
+                  SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      String text = todoController.text;
+                      setState(() {
+                        Todo newTodo = Todo(
+                          title: text,
+                          dateTime: DateTime.now(),
+                        );
+                        todos.add(newTodo);
+                      });
+                      todoController.clear();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff00d7f3),
+                      padding: EdgeInsets.all(14),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Flexible(
+                child: ListView(
+                  shrinkWrap: true,
                   children: [
-                    Expanded(
-                      child: Text(
-                        'Você possui ${todos.length} tarefas pendentes',
+                    for (Todo todo in todos)
+                      TodoListItem(
+                        todo: todo,
+                        onDelete: onDelete,
                       ),
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xff00d7f3),
-                          padding: EdgeInsets.all(14)),
-                      child: Text('Limpar Tudo'),
-                    )
                   ],
-                )
-              ],
-            ),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Você possui ${todos.length} tarefas pendentes',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: showDeleteTodosConfirmationDialog,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xff00d7f3),
+                        padding: EdgeInsets.all(14)),
+                    child: Text('Limpar Tudo'),
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
@@ -108,8 +108,71 @@ class _ToDoListPageState extends State<ToDoListPage> {
   }
 
   void onDelete(Todo todo) {
+    deletedTodo = todo;
+    deletedTodoPos = todos.indexOf(todo);
+
     setState(() {
       todos.remove(todo);
+    });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          'Tarefa ${todo.title} foi removida com sucesso!',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.grey[100],
+        action: SnackBarAction(
+          label: 'Desfazer',
+          textColor: Color(0xff00d7f3),
+          onPressed: () {
+            setState(() {
+              todos.insert(deletedTodoPos!, deletedTodo!);
+            });
+          },
+        ),
+        duration: const Duration(seconds: 5),
+      ),
+    );
+  }
+
+  void showDeleteTodosConfirmationDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Limpar tudo'),
+        content: Text('Você tem certeza que deseja apagar tudo?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            style: TextButton.styleFrom(
+              backgroundColor: Color(0xff00d7f3),
+              foregroundColor: Colors.white,
+            ),
+            child: Text('Cancelar'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              deleteAllTodos();
+            },
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+            ),
+            child: Text('Limpar tudo'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void deleteAllTodos() {
+    setState(() {
+      todos.clear();
     });
   }
 }
